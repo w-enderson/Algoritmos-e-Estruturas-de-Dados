860dcbe4fcca1760106f30cce0477c59499b8a62
Wenderson Juvenal <125162176+wenderson-juvenal@users.noreply.github.com>
2024-06-18 19:44:43 -0400

    Update heap.cpp

--- tree/heap.cpp
+++ tree/heap.cpp
@@ -9,35 +9,13 @@ private:
     vector<int> H;
     int n;
 
-    void inorder(int i) {
-        if (i <= n) {
-            inorder(2*i);
-            cout << H[i] << " ";
-            inorder(2*i + 1);
-        }
-    }
-    void preorder(int i) {
-        if (i <= n) {
-            cout << H[i] << " ";
-            preorder(2*i);
-            preorder(2*i + 1);
-        }
-    }
-    void posorder(int i) {
-        if (i <= n) {
-            posorder(2*i);
-            posorder(2*i + 1);
-            cout << H[i] << " ";
-        }
-    }
-
-    void bottomUp() {
+    void heapfyDown() {
         for (int i = n/2; i >= 1; i--) {
-            int k = i, v = H[k];
+            int k=i, v=H[k];
             bool heap = false;
             while (!heap && 2*k <= n) {
-                int j = 2*k;
-                if (j < n && H[j] < H[j+1]) { // >
+                int j = 2*k; // left
+                if (j<n && H[j]<H[j+1]) { // >
                     j++;
                 }
                 if (v >= H[j]) { // <=
@@ -51,26 +29,27 @@ private:
         }
     }
 
-    void topDown(int i) {
-        while (i > 1 && H[i/2] < H[i]) { // >
-            swap(H[i], H[i/2]);
-            i = i / 2;
+    void heapfyUp() {
+        for (int i=0; i<H.size(); i++){
+            // enquanto pai menor que filho,
+            // troca filho com pai
+            while (i>1 && H[i/2] < H[i]) { // >
+                swap(H[i], H[i/2]);
+                i = i/2;
+            }    
         }
     }
 
 public:
     Heap() {
-        H.push_back(0);  
+        H.push_back(0);
         n = 0;
     }
 
-    int getTop(){
-        if (n > 1) {
-            return H[1];
-        } else {
-            cout << "Heap vazia!" << endl;
-            return -1;
-        }
+    void setArr(vector<int> arr){
+        H = arr;
+        n = H.size()-1;
+        heapfyDown();
     }
     void printArray() {
         for (int i = 1; i <= n; i++) {
@@ -78,57 +57,43 @@ public:
         }
         cout << endl;
     }
-
-    // bottomUp
-    void remove() {
-        if (n != 1) {
-            swap(H[1], H[n]);
-            n--;
-            H.pop_back();
-            bottomUp();
+    int getTop(){
+        if (n > 0) {
+            return H[1];
         } else {
-            cout << "vazio" << endl;
+            cout << "Heap vazia!" << endl;
+            return -1;
         }
     }
 
-    // topDown
-    void insert(int value) {
-        H.push_back(value);
-        n++;
-        topDown(n);
+    // bottomUp
+    void remove() {
+        if (n>0) {
+            swap(H[1], H[n]);
+            n--;
+            heapfyDown();
+        }
     }
 
-    // tests
-    void testBottomUp(vector<int>& elements) {
-        cout << "Testing bottomUp heapify:" << endl;
-        H=elements;
-        n=H.size();
-        bottomUp();
-        printArray();
-        cout << endl;
-    }
+    void heapsort(){
+        while (n>1){
+            remove();
+        }
 
-    void testTopDown(vector<int>& elements) {
-        cout << "Testing topDown heapify:" << endl;
-        for (int elem : elements) {
-            insert(elem);
-            printArray();
+        for (int i=1; i<H.size(); i++){
+            cout << H[i] << " ";
         }
         cout << endl;
     }
 };
 
 int main() {
+    vector<int> arr = {0, 9,6,8,2,5,7};
+
     Heap heap;
-
-    vector<int> elements = {2, 9, 7, 6, 5, 8, 10};
-
-    heap.testBottomUp(elements);
-
-    // Reset heap for topDown test
-    heap = Heap();
-
-    heap.testTopDown(elements);
+    
+    heap.setArr(arr);
+    heap.heapsort();
 
     return 0;
 }
